4cb6fe50d82167d50c1f7d6840c8ba21e8d056a0
make <max.kerp@smail.inf.h-brs.de>
2013-04-17 17:25:29 +0200

    Commented the AddressListView

--- src/model/AddressList.java
+++ src/model/AddressList.java
@@ -3,6 +3,7 @@ package model;
 import java.io.Serializable;
 import java.util.LinkedList;
 
+// AddressList is just an subclass of an LinkedList<Address>
 public class AddressList extends LinkedList<Address> implements Serializable {
 	private static final long serialVersionUID = -8436170099085318899L;
 }
--- src/view/AddressListView.java
+++ src/view/AddressListView.java
@@ -19,10 +19,12 @@ import model.AddressList;
 
 @SuppressWarnings("serial")
 public class AddressListView extends JFrame {
-
+	
+	// member fields, the AddressList to display and a ListModel
 	private AddressList addressList;
 	private DefaultListModel listModel;
 
+	// create a AddressListView
 	public AddressListView(AddressList addressList) {
 		this.addressList = addressList;
 		init();
@@ -30,12 +32,15 @@ public class AddressListView extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-
+	
+	// Setting up the view
 	private void init() {
+
+		// setting Jframe (Window) title and layout
 		this.setTitle("Address-List");
-
 		this.setLayout(new GridBagLayout());
-
+		
+		// creating the constraints for the JScrollPane
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.fill = GridBagConstraints.HORIZONTAL;
 		constraints.gridx = 0;
@@ -43,17 +48,20 @@ public class AddressListView extends JFrame {
 		constraints.gridwidth = 2;
 		constraints.weighty = 0.9;
 
+		// creating the JScrollPane
 		listModel = new DefaultListModel();
 		JList list = new JList(listModel);
 		JScrollPane scrollpane = new JScrollPane(list);
-
+		
+		// adding the JScrollPane to the JFrame with its specific constraints
 		this.add(scrollpane, constraints);
-
+		
+		// creating a addButton and registering a new listener
 		JButton addButton = new JButton("Add address");
 		final AddressListView alv = this;
 		addButton.addActionListener(new ActionListener() {
 			/**
-			 * Wählen Sie für diese anonyme Klasse AddButtonActionListener als
+			 * Wï¿½hlen Sie fï¿½r diese anonyme Klasse AddButtonActionListener als
 			 * Klassenamen.
 			 */
 			@Override
@@ -63,16 +71,18 @@ public class AddressListView extends JFrame {
 			}
 		});
 
+		// changing the constraints to fit to the button
+		// adding the button to the JFrame
 		constraints.weighty = 0.1;
 		constraints.gridy = 1;
 		constraints.gridwidth = 1;
 		this.add(addButton, constraints);
 
+		// creating a saveButton abd registering a new listener
 		JButton saveButton = new JButton("Save all");
-
 		saveButton.addActionListener(new ActionListener() {
 			/**
-			 * Wählen Sie für diese anonyme Klasse SaveButtonActionListener als
+			 * Wï¿½hlen Sie fï¿½r diese anonyme Klasse SaveButtonActionListener als
 			 * Klassenamen.
 			 */
 			@Override
@@ -89,23 +99,29 @@ public class AddressListView extends JFrame {
 				}
 			}
 		});
-
+		
+		// changing the constraints to fit to the button
+		// adding the button to the JFrame
 		constraints.gridx = 1;
 		constraints.gridy = 1;
 		this.add(saveButton, constraints);
-
+		
+		// packing the whole window together
 		this.pack();
 	}
-
+	
+	// adding an address to the addressList-model
 	public void addAddress(Address address) {
 		addressList.add(address);
 		this.refreshAddressList();
 	}
 
+	// filling the ListModel with addresses
 	private void populateFields() {
 		refreshAddressList();
 	}
 
+	// getting the addresses from the model
 	private void refreshAddressList() {
 		listModel.removeAllElements();
 		for (Address address : addressList) {
